fb05727f87109f88112661560237c76d6694ce50
Maxim Sergeev <maxsergeev39+gh@gmail.com>
2023-04-22 20:07:15 +0300

    Refactor `bot.py`

--- bot.py
+++ bot.py
@@ -13,19 +13,25 @@ from tgbot.handlers.user import register_user
 
 
 async def set_bot_commands(bot: Bot) -> None:
+    """
+    Sets bot commands and saves them to bot config in readable format
+    :param bot:
+    :return:
+    """
     await bot.set_my_commands([
         BotCommand('weather', 'Узнать погоду'),
         BotCommand('convert', 'Конвертировать валюту'),
         BotCommand('cat', 'Случайная картинка с котиком'),
         BotCommand('poll', 'Создать опрос'),
         BotCommand('cancel', 'Отменить текущую команду'),
+        BotCommand('help', 'Вывести эту справку'),
     ])
 
     bot['config'].commands = '\n'.join([f"/{command[1]} - {descr[1]}"
                                         for command, descr in await bot.get_my_commands()])
 
 
-async def main(config: Config) -> None:
+async def run_bot(config: Config) -> None:
     bot = Bot(token=config.api_keys.bot, parse_mode='HTML')
     dp = Dispatcher(bot, storage=MemoryStorage())
     session: ClientSession = await bot.get_session()
@@ -44,17 +50,21 @@ async def main(config: Config) -> None:
         await session.close()
 
 
-if __name__ == '__main__':
+def main() -> None:
     try:
         configuration: Config = load_config(".env")
     except EnvError as e:
         logger.error(e)
     else:
         try:
-            asyncio.run(main(configuration))
+            asyncio.run(run_bot(configuration))
         except (KeyboardInterrupt, SystemExit):
             logger.info("Bot stopped")
         except exceptions.NetworkError:
             logger.error("Network error. Check internet connection")
         except Exception as e:
             logger.opt(exception=configuration.debug).critical(f"Unexpected error: {e}")
+
+
+if __name__ == '__main__':
+    main()
